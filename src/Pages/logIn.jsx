edefdda5bef1c7a0d6c5b801e35a1c68140f06da
Marco Korcak <marcokorcak@Marcos-MacBook-Air.local>
2023-03-21 19:39:07 -0400

    started login page

--- src/Pages/logIn.jsx
+++ src/Pages/logIn.jsx
@@ -1,9 +1,18 @@
 import Container from "../components/Container";
+import logo from "../image.png";
 
 
 const Login = () => {
     return (<Container>
-        <p>Login page</p>
+        <img className = 'a' src = {logo}></img>
+
+      <input className="a" name="userName" placeholder="User Name" />
+      
+       <input className="b" name="password" placeholder="Password" />
+     
+
+      <button type="submit">Log In</button>
+
     </Container>)
 }
 
