5913a4a865ffab94cfa42c52699b5704405fb9e9
Marco Korcak <marcokorcak@Marcos-MacBook-Air.local>
2023-03-22 13:49:26 -0400

    styling

--- src/Pages/viewCredit.jsx
+++ src/Pages/viewCredit.jsx
@@ -1,13 +1,72 @@
 import Container from "../components/Container";
 import React from 'react';
+import card1 from "../images/card1.png";
+import card2 from "../images/card2.png";
+
+
 
 
 const ViewCredit = () => {
-    return (<Container>
-                <div className="every">
 
-        <p>Credit page</p>
+    const debt = 1000;
+
+    const payBalance = () => {
+
+    }
+
+    return (<Container>
+
+        <div class="container">
+            <h1 className = "text-light text-center m-t 40 a" >Credit Cards</h1>
+            <div class="row">
+                <div class="col-lg-6 mb-4">
+                   
+                        <div class = " card_back">
+                        <div class="container_card">
+                        <img class="card-img-top y" src={card2} alt="" />
+
+                        <div class="card-body">
+                            <div className='text-center'>
+                                <h1>Current Debt: ${debt}</h1>
+
+                                <label className="m-r">Amount to pay:</label>
+                                <div class="credit_input">
+                                    <input className="c" name="pay_amt" placeholder="0.00" />
+                                </div>
+
+                            </div>
+
+                            <button className="y"> Pay Now</button>
+
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-4">
+                    <div class="card_back"  >
+                        <div class = "container_card">
+                        <img class="card-img-top x" src={card1} alt="" />
+
+                        <div class="card-body">
+                            <div className='text-center'>
+                                <h1>Current Debt: ${debt}</h1>
+
+                                <label className="m-r">Amount to pay:</label>
+                                <div class="credit_input">
+                                    <input className="c" name="pay_amt" placeholder="0.00" />
+                                </div>
+
+                            </div>
+
+                            <button className="y"> Pay Now</button>
+
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     </Container>)
 }
 
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -9,11 +9,13 @@ function NavBar() {
       <Navbar className="color-nav" fixed="top" >
         <Container>
           <Nav className="me-auto">
-            <Nav.Link href="/logIn">logIn</Nav.Link>
-            <Nav.Link href="/viewAccount">Account</Nav.Link>
-            <Nav.Link href="/viewCredit">Credit</Nav.Link>
-            <Nav.Link href="/makePayment">Transfer</Nav.Link>
-            <Nav.Link href="/settings">Settings</Nav.Link>
+
+            <Nav.Link href="/logIn" className= "text-light">LogIn</Nav.Link>
+            <Nav.Link href="/viewAccount" className= "text-light">Account</Nav.Link>
+            <Nav.Link href="/viewCredit" className= "text-light">Credit</Nav.Link>
+            <Nav.Link href="/makePayment" className= "text-light">Transfer</Nav.Link>
+            <Nav.Link href="/settings" className= "text-light">Settings</Nav.Link>
+        
           </Nav>
         </Container>
       </Navbar>     
