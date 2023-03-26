b9b264e2a7b50b7077826806559d406174f6d453
Marco Korcak <marcokorcak@Marcos-MacBook-Air.local>
2023-03-25 23:38:20 -0400

    styles

--- src/Pages/logIn.jsx
+++ src/Pages/logIn.jsx
@@ -1,42 +1,33 @@
 import Container from "../components/Container";
 import logo from "../images/logo1.png";
-import React from 'react';
-
+import React from "react";
 
 const Login = () => {
-    return (<Container>
-        <div class="col-lg-8 log">
+  return (
+    <Container>
+      <div class="col-lg-8 log">
+        <div className=" card_back_log">
+          <div className="container_card_log_a">
+            <div className="card-body">
+              <div className="text-center">
+                <img className="a" src={logo}></img>
 
-            <div className=" card_back_log">
-                <div className="container_card_log_a">
+                <input className="a" name="userName" placeholder="User Name" />
 
-                    <div className="card-body">
-                        <div className='text-center'>
-
-                            <img className='a' src={logo}></img>
-
-                            <input className="a" name="userName" placeholder="User Name" />
-
-                            <input className="b" name="password" placeholder="Password" />
-
-                        </div>
-
-                    </div>
-
-                </div>
+                <input className="b" name="password" placeholder="Password" />
+              </div>
             </div>
-
-            <div class="fill-button">
-
-                <button class="btn">
-                    <p className = "text-light font-weight-bold">
-                    Log In</p></button>
-            </div>
-
+          </div>
         </div>
 
-    </Container>)
-
-}
+        <div class="fill-button">
+          <button class="btn">
+            <p className="text-light font-weight-bold">Log In</p>
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
+};
 
 export default Login;
